b7e1832dbeb26b83cab31438218f5227eecbd4ee
Owen Erhabor <erhaborowen3@gmail.com>
2025-09-13 21:03:22 +0100

    chore: clean up and setup

--- app/login/_layout.tsx
+++ app/login/_layout.tsx
@@ -1,5 +1,6 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 
 export default function RootLayout() {
+  const router = useRouter();
   return <Stack screenOptions={{ headerShown: false }} />;
 }
