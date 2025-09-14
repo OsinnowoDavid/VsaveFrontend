8f82b03ce0cd5911b5be7216a84e360b52c4fd7f
Owen Erhabor <erhaborowen3@gmail.com>
2025-09-14 19:32:42 +0100

    feat: add onboarding screen including assets needed

--- app/auth/onBoarding/index.tsx
+++ app/auth/onBoarding/index.tsx
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Image, ImageBackground, Pressable, Text, View } from "react-native";
+import GradientText from "../../../components/GradientText";
+import "../../../global.css";
 
-export class loginOrSign extends Component {
-  render() {
-    return (
-      <View>
-        <Text className='bg-black text-white'> Login and sign up page</Text>
+export default function App() {
+  return (
+    <ImageBackground
+      source={require("../../../assets/images/onboarding-wallpaper.jpg")}
+      className="flex-1 justify-center items-center relative"
+      resizeMode="cover"
+    >
+      <Image
+        source={require("../../../assets/images/transparent-logo.png")}
+        className="absolute top-20"
+        resizeMode="cover"
+      />
+      <View className="flex flex-row gap-1 absolute top-56">
+        <Text className="text-2xl font-bold">Save Now,</Text>
+        <GradientText>Enjoy Later</GradientText>
       </View>
-    )
-  }
+      <View className="absolute bottom-28 w-[90%] gap-2.5">
+        <Pressable className="bg-white w-full py-3 rounded-lg">
+          <Text className="text-center font-semibold">Get Started</Text>
+        </Pressable>
+        <Pressable className="bg-transparent border-2 border-white w-full py-3 rounded-lg ">
+          <Text className="text-white text-center font-semibold">Login</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
 }
-
-export default loginOrSign
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   presets: [require("nativewind/preset")], // <-- Add this line
   content: [
-    './app/**/*.{js,tsx,ts,jsx}',
+    "./app/**/*.{js,tsx,ts,jsx}",
     // Add other directories if needed
   ],
+  safelist: ["text-growth-emerald"],
   theme: {
     extend: {},
   },
