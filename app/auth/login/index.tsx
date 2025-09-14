248ab53111f62842a6ae4ace4260872372f34021
Owen Erhabor <erhaborowen3@gmail.com>
2025-09-15 00:04:46 +0100

    feat: add login screen
    implement keyboardAvoidWrapper to effectively handle form in both android and ios

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
+import Button from "../../../components/Button";
+import FormField from "../../../components/FormField";
+import GradientText from "../../../components/GradientText";
+import KeyboardAvoidingWrapper from "../../../components/KeyboardAvoidWrapper";
+import PinInput from "../../../components/PinInput";
 
-export default function Login() {
+export default function App() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = () => {};
+
   return (
-    <View className="flex-1 bg-red-500 p-4 justify-center items-center">
-      <Text className="text-white text-lg font-bold">Login Screen</Text>
-    </View>
+    <ImageBackground
+      source={require("../../../assets/images/onboarding-wallpaper.jpg")}
+      className="flex-1 justify-around items-center relative border"
+      resizeMode="cover"
+    >
+      <Image
+        source={require("../../../assets/images/transparent-logo.png")}
+        className="absolute top-20"
+        resizeMode="cover"
+      />
+      <View className="flex flex-row gap-1 absolute top-56">
+        <Text className="text-2xl font-bold">Save Now,</Text>
+        <GradientText>Enjoy Later</GradientText>
+      </View>
+      <KeyboardAvoidingWrapper>
+        <View className="px-6 py-8 bg-white w-full rounded-t-xl">
+          <Text className="text-2xl font-bold pb-4 mb-8 text-center border-b-[0.3px] border-gray-500">
+            Login
+          </Text>
+          <FormField
+            label="Email"
+            value={form.email}
+            onChangeText={(email) => setForm({ ...form, email })}
+            placeholder="you@example.com"
+          />
+
+          <PinInput />
+          <Button
+            input="Login"
+            onPress={() => {
+              handleSubmit;
+            }}
+            color="text-white"
+          />
+        </View>
+      </KeyboardAvoidingWrapper>
+    </ImageBackground>
   );
 }
---
+++ components/KeyboardAvoidWrapper.tsx
@@ -0,0 +1,35 @@
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+export default function KeyboardAvoidingWrapper({ children }) {
+  if (Platform.OS === "android") {
+    return (
+      <KeyboardAwareScrollView
+        contentContainerStyle={{
+          alignItems: "flex-end",
+          flexGrow: 1,
+        }}
+        enableOnAndroid
+        keyboardShouldPersistTaps="handled"
+        horizontal
+      >
+        {children}
+      </KeyboardAwareScrollView>
+    );
+  } else {
+    return (
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        style={{
+          flex: 1,
+          justifyContent: "flex-end",
+          margin: 0,
+          borderWidth: 1,
+          width: "100%",
+        }}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    );
+  }
+}
