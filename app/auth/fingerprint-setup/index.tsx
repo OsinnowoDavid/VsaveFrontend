50ba499b0763c4f284efb8ae5e7c295ab9f95ace
Owen Erhabor <erhaborowen3@gmail.com>
2025-09-23 12:23:12 +0100

    refactor(forms, ui): Improve styling consistency and reduce code duplication
    This commit addresses two key areas of improvement:
    
    - **Tailwind JIT Configuration:** Updated the `tailwind.config.js` `content` array to correctly include component files. This ensures that the JIT compiler can detect and generate all necessary utility classes, eliminating the need for manual `safelist` entries for components.
    
    - **Form Refactoring:** Introduced a new `FormWrapper` component to encapsulate the shared UI for form headings and layouts. This removes redundant styling code from individual form components, improving consistency and making the codebase more modular and maintainable.

--- app/auth/fingerprint-setup/index.tsx
+++ app/auth/fingerprint-setup/index.tsx
@@ -3,51 +3,46 @@ import { useState } from "react";
 import { Image, Switch, Text, View } from "react-native";
 import ScreenWrapper from "../../../components/AuthScreenWrapper";
 import Button from "../../../components/Button";
-import KeyboardAvoidingWrapper from "../../../components/KeyboardAvoidWrapper";
+import FormWrapper from "../../../components/FormWrapper";
 
 export default function ExtraLoginSetupScreen() {
   const [isEnabled, setIsEnabled] = useState(true);
   const router = useRouter();
   return (
     <ScreenWrapper>
-      <KeyboardAvoidingWrapper>
-        <View className="px-6 py-8 bg-white w-full rounded-t-3xl">
-          <Text className="text-2xl font-bold pb-4 mb-8 text-center border-b-[0.3px] border-gray-500">
-            Extra Login Options
+      <FormWrapper heading="Extra Login Options">
+        <Text className="text-right text-green-700">skip</Text>
+        <Image
+          source={require("../../../assets/icons/fingerprint.png")}
+          className="w-20 h-20 mx-auto"
+        />
+        <View className="flex items-center mt-4">
+          <Text className="font-bold text-lg">Use Fingerprint To Access</Text>
+          <Text className="text-gray-600">
+            Login fast and quick with biometric
           </Text>
-          <Text className="text-right text-green-700">skip</Text>
-          <Image
-            source={require("../../../assets/icons/fingerprint.png")}
-            className="w-20 h-20 mx-auto"
-          />
-          <View className="flex items-center mt-4">
-            <Text className="font-bold text-lg">Use Fingerprint To Access</Text>
-            <Text className="text-gray-600">
-              Login fast and quick with biometric
-            </Text>
+        </View>
+        <View className="flex flex-row justify-between mt-6 my-4">
+          <View className="flex items-center justify-center">
+            <Text className="text-gray-600">Activate fingerprint</Text>
           </View>
-          <View className="flex flex-row justify-between mt-6 my-4">
-            <View className="flex items-center justify-center">
-              <Text className="text-gray-600">Activate fingerprint</Text>
-            </View>
-            <Switch
-              value={isEnabled}
-              onValueChange={setIsEnabled}
-              thumbColor={isEnabled ? "#fff" : "#ccc"}
-              trackColor={{ false: "#767577", true: "#15803d" }}
-            />
-          </View>
-          <Button
-            input="Use Pin Code"
-            onPress={() => {
-              router.push("/auth/pincode-setup");
-            }}
-            bg=""
-            color="text-green-700"
-            border="border border-green-700"
+          <Switch
+            value={isEnabled}
+            onValueChange={setIsEnabled}
+            thumbColor={isEnabled ? "#fff" : "#ccc"}
+            trackColor={{ false: "#767577", true: "#15803d" }}
           />
         </View>
-      </KeyboardAvoidingWrapper>
+        <Button
+          input="Use Pin Code"
+          onPress={() => {
+            router.push("/auth/pincode-setup");
+          }}
+          bg=""
+          color="text-green-700"
+          border="border border-green-700"
+        />
+      </FormWrapper>
     </ScreenWrapper>
   );
 }
