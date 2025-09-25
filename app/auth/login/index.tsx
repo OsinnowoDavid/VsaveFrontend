f0471557d68c3f7de2381e2517ad78a104bc9b5d
Osinnowo David <davidadewale151@gmail.com>
2025-09-25 16:23:32 +0100

    airtime and data

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -3,7 +3,6 @@ import { Text, View } from "react-native";
 import ScreenWrapper from "../../../components/AuthScreenWrapper";
 import Button from "../../../components/Button";
 import FormField from "../../../components/FormField";
-import KeyboardAvoidingWrapper from "../../../components/KeyboardAvoidWrapper";
 import PinInput from "../../../components/PinInput";
 
 export default function LoginScreen() {
@@ -16,7 +15,7 @@ export default function LoginScreen() {
 
   return (
     <ScreenWrapper>
-      <KeyboardAvoidingWrapper>
+  
         <View className="px-6 py-8 bg-white w-full rounded-t-3xl">
           <Text className="text-2xl font-bold pb-4 mb-8 text-center border-b-[0.3px] border-gray-500">
             Login
@@ -37,7 +36,7 @@ export default function LoginScreen() {
             color="text-white"
           />
         </View>
-      </KeyboardAvoidingWrapper>
+      
     </ScreenWrapper>
   );
 }
